8e7bccf78d1998407d913fa636192964f4f99bd4
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-08-22 08:28:22 +0530

    developed signin page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+import Signin from "../../components/signin/signin.component";
+
+const SignInSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <Signin />
+  </div>
+);
+
+export default SignInSignUpPage;
