8baba016b3657c6c8eb940836279480cb00c3d79
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-08-25 12:22:14 +0530

    implemented signinWithEmailAndPassword which I missed Earlier

--- src/components/sign-in/signin.component.jsx
+++ src/components/sign-in/signin.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./signin.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils.js";
 
 class SignInComponent extends React.Component {
   constructor(props) {
@@ -14,9 +14,18 @@ class SignInComponent extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+    
   };
 
   handleChange = event => {
