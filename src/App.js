8cdd2b93ec3108143338839a0d8d342cc783ffc3
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-08-29 09:00:05 +0530

    created base checkout page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.co
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
 
@@ -47,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={ CheckoutPage } />
           <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/" /> : <SignInSignUpPage />} />
         </Switch>
       </div>
