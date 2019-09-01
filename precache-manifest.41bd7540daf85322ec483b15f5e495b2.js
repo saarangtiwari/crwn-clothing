07fb577e4983d3807215310078d0dd975500ae76
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-09-01 12:29:12 +0530

    Updates

---
+++ precache-manifest.41bd7540daf85322ec483b15f5e495b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1612845ec3506137bea878fb7f8bb476",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2c5ad59e852dd48dfd59",
+    "url": "/crwn-clothing/static/css/main.e8f807b7.chunk.css"
+  },
+  {
+    "revision": "f938609e0ca9d0e5c6c3",
+    "url": "/crwn-clothing/static/js/2.642095c5.chunk.js"
+  },
+  {
+    "revision": "2c5ad59e852dd48dfd59",
+    "url": "/crwn-clothing/static/js/main.7e45429f.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
