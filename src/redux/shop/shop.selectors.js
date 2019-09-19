c40f5832e8914a6c25e86aed602cf9e2ed28afbc
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-09-19 12:03:14 +0530

    working on routes inside the collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,10 +1,24 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+}
+
 const selectShop = state => state.shop;
 
-const selectShopCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
 )
 
-export default selectShopCollections;
+export const selectCollection = collectionUrlParam => createSelector(
+  [selectCollections],
+  collections =>
+    collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+);
+
+// export default selectShopCollections;
